4c34e13998fc9011685c4dbe0ed9058f746e294d
Лисицын Владимир <144523509+VOR2004@users.noreply.github.com>
2024-11-20 22:48:00 +0300

    Revert "hw1"

--- app/src/androidTest/java/ru/ucheba/myapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/ucheba/myapplication/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ru.ucheba.hw1
+package ru.ucheba.myapplication
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("ru.ucheba.hw1", appContext.packageName)
+        assertEquals("ru.ucheba.myapplication", appContext.packageName)
     }
 }
